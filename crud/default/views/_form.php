0d8df72d5fe0e63194d48ed36ec645f38771bb19
amer <amer@gmail.com>
2022-04-03 16:28:55 +0200

    fix form

--- crud/default/views/_form.php
+++ crud/default/views/_form.php
@@ -54,7 +54,12 @@ foreach ($relations as $name => $rel) {
 
 <?php foreach ($generator->tableSchema->getColumnNames() as $attribute) {
     if (!in_array($attribute, $generator->skippedColumns)) {
-        echo " <div class=\"col-md-4\">   <?= " . $generator->generateActiveField($attribute, $generator->generateFK()) . " ?> </div>\n\n";
+        $i=0;
+        if ($i % 2 != 0) { echo "<div class='row'>" ;}
+            echo " <div class=\"col-md-4\">   <?= " . $generator->generateActiveField($attribute, $generator->generateFK()) . " ?> </div>\n\n";
+        if ($i % 2 != 0) { echo  "</dev>" ; }
+
+        $i++;
     }
 } ?>
 
